e6a16798fff1f30c12727eac265138578a0f5533
Ben Oh <moh16@my.bcit.ca>
2025-10-19 02:26:40 -0700

    done

--- server1/lang/en/en.js
+++ server1/lang/en/en.js
@@ -2,6 +2,34 @@
  * Represents user interface strings for server 1.
  */
 export class UserInterfaceString {
+    static INSERT_BUTTON = "Insert"
+
+    static SUBMIT_QUERY_BUTTON = "Submit"
+
+    static INVALID_QUERY = "Only SELECT or INSERT query allowed"
+
+    static EMPTY_QUERY = "Please enter a query"
+
+    static GET_DOMAIN = "https://comp4537-lab4-6ylr.onrender.com/GET/"
+
+    static POST_DOMAIN = "https://comp4537-lab4-6ylr.onrender.com/POST/"
+
+    static GET_SUCCESS(data) {
+        let message = `Successfully retrieved:`;
+        data.result.forEach( (row) => {
+            message += `\n ${JSON.stringify(row)}`
+        } )
+        return message
+    }
+
+    static POST_SUCCESS = "Successfully inserted to the database"
+
+
+
+
+
+
+
     static SEARCH_BUTTON = "Search"
     static SEARCH_RESULT(requestCount, query) {
         return `Request# ${requestCount}, word '${query}' not found!`
---
+++ server1/script.js
@@ -0,0 +1,79 @@
+import { UserInterfaceString } from "./lang/en/en.js"
+
+const insertButton = document.getElementById("insert-button");
+const submitQueryButton = document.getElementById("query-button");
+const queryArea = document.getElementById("query-area");
+const feedbackArea = document.getElementById("feedback")
+
+insertButton.innerHTML = UserInterfaceString.INSERT_BUTTON;
+submitQueryButton.innerHTML = UserInterfaceString.SUBMIT_QUERY_BUTTON;
+
+
+insertButton.addEventListener("click", async () => {
+    const query = `INSERT INTO patient (name, dateOfBirth) VALUES 
+    ('Sara Brown', '1981-01-01'),
+    ('John Smith', '1941-01-01'),
+    ('Jack Ma', '1961-01-30'),
+    ('Elon Musk', '1999-01-01')`;
+
+    await QueryHandler.executeQuery(query)
+
+})
+
+submitQueryButton.addEventListener("click", async () => {
+    const query = queryArea.value.trim()
+    if (!query) {
+        feedbackArea.innerHTML = UserInterfaceString.EMPTY_QUERY
+        return
+    }
+    await QueryHandler.executeQuery(query)
+})
+
+
+
+class QueryHandler {
+    static async executeQuery(query) {
+        const queryUpper = query.toUpperCase();
+        if(queryUpper.startsWith("SELECT")) {
+            await QueryHandler.sendGetRequest(query)
+
+        } else if (queryUpper.startsWith("INSERT")) {
+            await QueryHandler.sendPostRequest(query)
+
+        } else {
+            feedbackArea.innerHTML = UserInterfaceString.INVALID_QUERY;
+        }
+
+    }
+
+    static async sendGetRequest(query) {
+        const encodedQuery = encodeURIComponent(query)
+        const response = await fetch(`${UserInterfaceString.GET_DOMAIN}?query=${encodedQuery}`)
+        if (response.ok) {
+            const data = await response.json();
+            feedbackArea.textContent = UserInterfaceString.GET_SUCCESS(data);
+        } else {
+            const data = await response.json();
+            feedbackArea.textContent = data.error;
+        }
+    }
+
+    static async sendPostRequest(query) {
+        const response = await fetch(UserInterfaceString.POST_DOMAIN, {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify({theQuery: query})
+        })
+        if (response.ok) {
+            const successInsertMessage = UserInterfaceString.POST_SUCCESS;
+            feedbackArea.textContent = successInsertMessage;
+        } else {
+            const data = await response.json();
+            feedbackArea.textContent = data.error;
+        }
+
+    }
+
+}
